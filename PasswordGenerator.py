919d59aafa853b7dbf08475f970d3d5fe897eb97
ay-pawn Llana <natellana07@gmail.com>
2025-02-12 18:42:54 +0000

    password

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,6 @@
 import random
 import string
+import requests
 
 #function that generates a single random character
 def random_character():
@@ -20,9 +21,29 @@ generate_strong_password()
 
 def fetch_word():
     url ="https://random-word-api.herokuapp.com/word?length=6"
-
     response = requests.get(url)
     word = response.json()[0]
     return word
 
-print(fetch_word())
+def replaceletters(word):
+    word = word[0].upper() + word[1:]
+    if "a" in word:
+        word = word.replace("a", "@")
+    if "e" in word:
+        word = word.replace("e", "3")
+    if "i" in word: 
+        word = word.repalce("i", "1")
+    if "s" in word:
+        word = word .replace("s", "$")
+    return word
+
+def generate_weaker_password():
+    word1 = fetch_word()
+    word2 = fetch_word()
+    word1 = replaceletters(word1)
+    word2 = replaceletters(word2)
+    password = word1 + word2
+    return password
+
+
+print(generate_weaker_password())
